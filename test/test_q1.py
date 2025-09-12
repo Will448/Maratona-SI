1dfc4fe9abcb599d631f9c9dedf2e7d3d8fdb011
Willian <92097335+Will448@users.noreply.github.com>
2025-09-12 19:52:42 -0300

    Update test_q1.py

--- test/test_q1.py
+++ test/test_q1.py
@@ -1,1 +1,15 @@
 
+import subprocess
+
+def run_program(command, input_data):
+    result = subprocess.run(
+        command, input=input_data.encode(),
+        capture_output=True, text=True
+    )
+    return result.stdout.strip()
+
+def test_case1():
+    assert run_program(["python3", "questão1/main.py"], "2 3") == "5"
+
+def test_case2():
+    assert run_program(["python3", "questão1/main.py"], "10 20") == "30"
